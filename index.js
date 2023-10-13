cd57c117716a7dbab85566ebbbcac6c83316eb60
Brahma behera <beherabrahmananda77@gmail.com>
2023-10-13 11:32:04 +0530

    added new indexfile

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.post('/screenshot', async (req, res) => {
+app.get('/screenshot', async (req, res) => {
   try {
-    const url = req.body.url;
+    const { url } = req.query;
 
-    const browser = await puppeteer.launch({
-        headless: true, // Use the new headless mode
-        args: ['--no-sandbox', '--disable-setuid-sandbox'], // Additional options (if needed)
-      });
-    
-      const page = await browser.newPage();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
     await page.goto(url);
-    
-    // Adjust the viewport and wait for page content to load
-    await page.waitForTimeout(1000); 
+    const screenshot = await page.screenshot();
 
-    const screenshot = await page.screenshot({ fullPage: true });
-    
     await browser.close();
 
-    res.contentType('image/png');
+    res.setHeader('Content-Type', 'image/png');
     res.send(screenshot);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'An error occurred while capturing the screenshot.' });
+    res.status(500).send('Error capturing the screenshot.');
   }
 });
 
